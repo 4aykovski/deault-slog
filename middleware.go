dd0c81e4f5b24547f7b7e97d0538eb3dd62e5ee2
4aykovski <kan89kan@gmail.com>
2024-10-23 09:20:17 +0700

    feat: add empty handler middleware

---
+++ middleware.go
@@ -0,0 +1,40 @@
+package defaultslog
+
+import (
+	"context"
+	"log/slog"
+)
+
+type LogCtxKey struct{}
+
+type HandlerMiddleware struct {
+	next slog.Handler
+}
+
+func NewHandleMiddleware(next slog.Handler) *HandlerMiddleware {
+	return &HandlerMiddleware{next: next}
+}
+
+func (h *HandlerMiddleware) Handle(ctx context.Context, rec slog.Record) error {
+	// if logArgs, ok := ctx.Value(LogCtxKey).(LogCtx); ok {
+	//
+	// 	for logKey, logValue := range logArgs.Args {
+	// 		rec.Add(logKey, logValue)
+	// 	}
+	//
+	// }
+	//
+	return h.next.Handle(ctx, rec)
+}
+
+func (h *HandlerMiddleware) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &HandlerMiddleware{next: h.next.WithAttrs(attrs)}
+}
+
+func (h *HandlerMiddleware) WithGroup(name string) slog.Handler {
+	return &HandlerMiddleware{next: h.next.WithGroup(name)}
+}
+
+func (h *HandlerMiddleware) Enabled(ctx context.Context, rec slog.Level) bool {
+	return h.next.Enabled(ctx, rec)
+}
